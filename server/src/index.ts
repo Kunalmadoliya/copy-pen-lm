a594c5d01cca1e951c2f5fe6a9df6e57c499a3b5
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-08-19 20:20:03 +0530

    feat: implement workspace management with CRUD operations
    - Added workspace controller for handling requests related to workspaces.
    - Created workspace repository for database interactions.
    - Introduced workspace services to encapsulate business logic.
    - Implemented validation schemas for workspace creation and updates using Zod.
    - Added error handling middleware to manage application errors.
    - Integrated CORS support for cross-origin requests.
    - Updated Prisma schema and migrations to include workspace-related models and enums.
    - Registered workspace routes in the main application.
    - Enhanced the application structure with utility functions for async handling and Zod error processing.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,23 @@ import "dotenv/config";
 import express from "express";
 import {auth} from "./lib/auth.js";
 import {toNodeHandler} from "better-auth/node";
+import cors from "cors";
+import {env} from "prisma/config";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
 
 const app = express();
+const PORT = process.env.PORT || 8081;
+const clientUrl = process.env.CLIENT_URL || "http://localhost:3000"; // Default to localhost:3000 if CLIENT_URL is not set
 
 app.all("/api/auth/*", toNodeHandler(auth));
 
-const PORT = process.env.PORT || 8081;
+app.use(
+  cors({
+    origin: clientUrl,
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -16,6 +27,9 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.send({status: "ok"});
 });
+
+registerRoutes(app);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
